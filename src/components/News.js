17778961e0bf0d69fc6a8cb569af8f760f084751
Vimal57 <vimals@zignuts.com>
2022-07-08 16:15:17 +0530

    added class, constructor, componentDidMount, and rendering news by fetching api

--- src/components/News.js
+++ src/components/News.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+    }
+  }
+
+  async componentDidMount() {
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=8e49a9e0a89642b8b5a26f02a918d315";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({articles: parsedData.articles})
+  }
+
   render() {
     return (
-      <div>
-        This is news component.
-        <NewsItem/>
+      <div className='container my-3'>
+        <h1>Fresh News - Top Headlines</h1>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return <div className="col-md-4" key={element.url}>
+              <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url}/>
+            </div>
+          })}
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
   render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
-      <div>
-        This is news item.
+      <div className='my-3'>
+            <div className="card" style={{width: "18rem"}}>
+                <img src={imageUrl?imageUrl:"https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">{title?title:""}</h5>
+                        <p className="card-text">{description?description:""}</p>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                    </div>
+            </div>
       </div>
     )
   }
